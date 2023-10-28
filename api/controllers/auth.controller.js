d6ac61124f194e35e4a966e613ff915052270ccc
jheart-vic <adebowalevictorjheart@gmail.com>
2023-10-28 03:21:06 +0100

    add delete and sign out functionalities

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
-    const hashPassword = await bcrypt.hashSync(password, 10);
+    const hashPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
     res.status(201).json("User created successfully");
@@ -22,8 +22,8 @@ export const signin = async (req, res, next) => {
     const validPassword = bcrypt.compareSync(password, validUserName.password);
     if (!validPassword) return next(new Error("Wrong credentials"));
     const token = jwt.sign({ id: validUserName._id }, process.env.JWT_KEY);
-    const { password: pass, ...user } = validUserName._doc;
-    res.cookie("token", token, { httpOnly: true }).status(200).json(user);
+    const { password: pass, ...rest } = validUserName._doc;
+    res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
@@ -36,18 +36,27 @@ try {
 const user=await User.findOne({email: req.body.email});
 if(user){
 const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
-const { password: pass, ...user } = user._doc;
-res.cookie("token", token, { httpOnly: true }).status(200).json(user);
+const { password: pass, ...rest } = user._doc;
+res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
 }else{
 const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
 const hashPassword = bcrypt.hashSync(generatedPassword, 10);
 const newUser = new User({ username: req.body.name.split(" ").join("").toLocaleLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashPassword, avatar: req.body.photo});
 await newUser.save();
 const token = jwt.sign({ id: newUser._id }, process.env.JWT_KEY);
-const { password: pass, ...user } = newUser._doc;
-res.cookie("token", token, { httpOnly: true }).status(201).json(user);
+const { password: pass, ...rest } = newUser._doc;
+res.cookie("token", token, { httpOnly: true }).status(201).json(rest);
 }
 } catch (error) {
 next(error);
 }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('token');
+    res.status(200).json('You has been logged out!');
+  } catch (error) {
+    next(error);
+  }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,9 @@
 import {useRef, useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {
-  updateUserStart, updateUserSuccess, updateUserFailure
+  updateUserStart, updateUserSuccess, updateUserFailure,
+   deleteUserStart, deleteUserFailure, deleteUserSuccess,
+   signOutUserStart
 } from '../redux/user/userSlice';
 import {
   getDownloadURL,
@@ -82,6 +84,40 @@ const Profile = () => {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignOut = async () => {
+
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className="p-2 max-w-lg mx-auto mt-24">
       <h1 className="text-3xl font-semibold text-center">Profile</h1>
@@ -102,7 +138,7 @@ const Profile = () => {
         <p className="text-sm self-center">
           {fileUpLoadError
             ? <span className="text-red-700">
-                Error uploading image(image must be less than 5mb)
+                Error uploading image(image must be less than 2mb)
               </span>
             : filePercent > 0 && filePercent < 100
                 ? <span className="text-slate-700">
@@ -144,10 +180,10 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer hover:bg-red-900 hover:text-white hover:p-1 hover:rounded-md">
+        <span onClick={handleDeleteUser}  className="text-red-700 cursor-pointer hover:bg-red-900 hover:text-white hover:p-1 hover:rounded-md">
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer hover:bg-red-900 hover:text-white hover:p-1 hover:rounded-md">
+        <span onClick={handleSignOut}className="text-red-700 cursor-pointer hover:bg-red-900 hover:text-white hover:p-1 hover:rounded-md" >
           Sign Out
         </span>
       </div>
